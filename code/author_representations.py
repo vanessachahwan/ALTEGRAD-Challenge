fe9ad74676ff025f39447ac8f07e284174f78948
BertrandPaturel <48726309+BertrandPaturel@users.noreply.github.com>
2021-02-25 19:29:46 +0100

    udpate embedding
    added mean, weighted mean and most popular

--- code/author_representations.py
+++ code/author_representations.py
@@ -14,30 +14,85 @@ for l in f:
     d[l.split(":")[0]] = auth_paps
 f.close()
 
-# read the embeddings of each paper
-f = open("../data/paper_embeddings.txt","r")
-papers = {}
-s = ""
-pattern = re.compile(r'(\s){2,}')
-for l in f:
-    if(":" in l and s!=""):
-        papers[s.split(":")[0]] = np.array(ast.literal_eval(re.sub(pattern, ',', s.split(":")[1]).replace(" ",",")))
-        s = l.replace("\n","")
-    else:
-        s = s+" "+l.replace("\n","")
-    
-f.close()
+def most_popular_paper(author):
+    most_popular = np.zeros(64)
+    best_popularity = 0
+    for paper in d[author]:
+        popularity = 0
+        indicatrice = 0
+        for co_author in G.neighbors(int(author)):
+            if paper in d[str(co_author)]:
+                popularity +=1
+        if popularity > best_popularity:
+            try:
+                most_popular = papers[paper]
+            except:
+                indicatrice = 1
+                continue
+            best_popularity = indicatrice*best_popularity + (1-indicatrice)*popularity
 
-# the author representation is set to be the average of its papers' representations
+    return most_popular
+        
+def weighted_mean(author):
+    epsilon = 1e-10
+    output = np.zeros(64)
+    sum_of_popularity = 0
+    for paper in d[author]:
+        popularity = 0
+        indicatrice = 1
+        for co_author in G.neighbors(int(author)):
+            if paper in d[str(co_author)]:
+                popularity +=1
+        try:
+            output = papers[paper] * popularity
+        except:
+            indicatrice = 0
+            continue
+        sum_of_popularity += indicatrice*popularity
+    output = output / (sum_of_popularity + epsilon)
+    return output
+
+#choose your papor embedding :
+sum_ = True
+weighted_mean = False
+mean = False
+most_popular = False
+
+
+# the author representation is set to be most popular of its papers' representations
 pattern = re.compile(r'(,){2,}')
-df = open("../data/author_embedding_64.csv","w")
-for author in d:    
+df = open("author_embedding_most_popular_64.csv","w")
+for author in d:
+    # if you wish to have that the authors'vector corresponds to the vector of its most popular paper
+    if most_popular:
+        v = most_popular_paper(author)
+    
+    # if you wish to have that the authors'vector corresponds to the vector of its weighted mean
+    if weighted_mean:
+        v = weighted_mean(author)
+        
+    df.write(author+","+",".join(map(lambda x:"{:.8f}".format(round(x, 8)), v))+"\n"
+        
+    if sum_:
     v = np.zeros(64)
     for paper in d[author]:
         try:
             v += papers[paper]
         except:
             continue
+     
+    if mean:
+        v = np.zeros(256)
+        c = 0
+        for paper in d[author]:
+            try:
+                v+=papers[paper]
+                c+=1
+            except:
+                continue
+        if(c==0):
+            c=1
+    
     df.write(author+","+",".join(map(lambda x:"{:.8f}".format(round(x, 8)), v))+"\n")
     
 df.close()
