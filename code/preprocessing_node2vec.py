fc642b202d44c81b92d4e29f0495feb5c97c064d
Vanessa Chahwan <71970769+vanessachahwan@users.noreply.github.com>
2021-03-01 08:43:40 +0100

    Create preprocessing_node2vec.py

---
+++ code/preprocessing_node2vec.py
@@ -0,0 +1,40 @@
+import os
+import pandas as pd
+import numpy as np
+from gensim.models import KeyedVectors
+import networkx as nx
+import ast
+import re
+
+node2vec_input = "../data/node2vec_sg.nodevectors"
+node2vec_output = "../data/author_node2vec_sg.csv"
+
+# read the file to create a list of authors
+G = nx.read_edgelist('collaboration_network.edgelist', delimiter=' ', nodetype=int)
+
+authors = list(G.nodes())
+
+# read the node2vec embeddings of each paper
+embeddings = KeyedVectors.load_word2vec_format(node2vec_input)
+
+# To choose between NAN or vector of zero:
+NAN = True
+
+
+# the author node2vec embeddings is set to be the sum of its papers' node2vec embeddings
+pattern = re.compile(r'(,){2,}')
+df = open(node2vec_output,"w")
+for author in authors:
+    
+    try:
+        v = embeddings[str(author)]
+    except:
+        continue
+       
+    if NAN:
+        if not v.any():
+            v = np.nan * np.ones(64)
+    
+    df.write(str(author)+","+",".join(map(lambda x:"{:.16f}".format(x), v))+"\n")
+    
+df.close()
